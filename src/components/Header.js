1692478b9887d217a26ee69935a38cfef8bb75c3
krishnadevz <krishnakakade77@gmail.com>
2021-06-25 18:10:47 +0530

    reponsive toggle menus

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,21 +3,21 @@ import Menu from "./svg/bars-solid.svg";
 import Close from "./svg/times-solid.svg";
 import CartIcon from "./svg/shopping-cart-solid.svg";
 import { Link } from "react-router-dom";
-import './css/Header.css';
+import "./css/Header.css";
 export default class Header extends Component {
+  state = {
+    toggle: false,
+  };
 
-    state = {
-        toggle: false
-    }
-
-    menuToggle = () =>{
-        this.setState({toggle: !this.state.toggle})
-    }
+  menuToggle = () => {
+    this.setState({ toggle: !this.state.toggle });
+  };
   render() {
-    const {toggle}=this.state;
+    const { toggle } = this.state;
+
     return (
       <header>
-        <div>
+        <div className="menu" onClick={this.menuToggle}>
           <img src={Menu} alt="" width="20" />
         </div>
         <div className="logo">
@@ -26,23 +26,37 @@ export default class Header extends Component {
           </h1>
         </div>
         <nav>
-          <ul className={toggle ? "toggle":""} >
-            <li>Home</li>
-            <li>New Releases</li>
-            <li>Men</li>
-            <li>Women</li>
-            <li>Kids</li>
-            <li>Sale</li>
-            <li>Collections</li>
+          <ul className={toggle ? "toggle" : ""}>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/">New Releases</Link>
+            </li>
+            <li>
+              <Link to="/">Men</Link>
+            </li>
+            <li>
+              <Link to="/">Women</Link>
+            </li>
+            <li>
+              <Link to="/">Kids</Link>
+            </li>
+            <li>
+              <Link to="/">Sale</Link>
+            </li>
+            <li>
+              <Link to="/">Collections</Link>
+            </li>
             <li className="close">
               <img src={Close} alt="" width="20" />
             </li>
           </ul>
           <div className="nav-cart">
             <span>0</span>
-            <Link to= "/cart">
-            <img src={CartIcon} alt="" width="20" />
-</Link>
+            <Link to="/cart">
+              <img src={CartIcon} alt="" width="20" />
+            </Link>
           </div>
         </nav>
       </header>
