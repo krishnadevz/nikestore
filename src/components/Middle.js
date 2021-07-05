1e13b5529f31b55cb75eed3e276fe691fead6982
krishnadevz <krishnakakade77@gmail.com>
2021-07-05 11:31:28 +0530

    more improvements in nikestore

--- src/components/Middle.js
+++ src/components/Middle.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
-import './css/Middle.css'
+import './css/Middle.css';
+import Arrow from './svg/rightarrow.svg'
 export default class Middle extends Component {
   render() {
     return (
-      <div id="row">
-        <h1>Men's Trainers & Shoes</h1>
-        <ul>
+      <div >
+        <p>Home page   <img src={Arrow} alt="" width="12" />     Men  
+             <img src={Arrow} alt="" width="12" />    Shoes  <p className="para">Men's Trainers & Shoes<p>--490 results</p></p> </p>
+             
+              
+        <h1 className="h1">Men's Trainers & Shoes</h1>
+        <ul id="row">
           <lu>
             Running<select></select>
           </lu>
